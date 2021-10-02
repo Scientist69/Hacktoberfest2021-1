8bec32dc1148a1e4af064e52071c0d1a51b2f515
UTKARSH AGRAWAL <71021450+Scientist69@users.noreply.github.com>
2021-10-02 23:47:34 +0530

    inverted_pattern_Scientist69.cpp

--- C++/inverted_pattern.cpp
+++ C++/inverted_pattern.cpp
@@ -1,16 +1,24 @@
+/*
+    1 2 3 4 5
+    1 2 3 4
+    1 2 3
+    1 2
+    1
+*/
+
 #include<iostream>
 using namespace std;
 int main(){
     int n;
     cin>>n;
 
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=n-i+1;j++){
-            if(j>n-i+1){
+    for(int i=1;i<=n;i++){ // for rows
+        for(int j=1;j<=n-i+1;j++){ // for columns
+            if(j>n-i+1){ // printing spaces
                 cout<<"  ";
             }
-            else{
-                cout<<j<<" ";
+            else{ // printing values
+                cout<<j<<" "; 
             }
         }
         cout<<endl;
